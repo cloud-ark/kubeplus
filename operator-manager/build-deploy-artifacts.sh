4050fe1b3a0d35c755f3cd02c7c9e48287b2e9b5
Devdatta Kulkarni <devdattakulkarni@gmail.com>
2018-11-28 14:07:43 -0600

    Refactoring
    - Updated Postgres example steps
    - Updated build scripts for operator-deployer and operator-manager

--- operator-manager/build-deploy-artifacts.sh
+++ operator-manager/build-deploy-artifacts.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+rm -f operator-manager
 export GOOS=linux; go build .
 cp operator-manager ./artifacts/deployment/operator-manager
 docker build -t lmecld/operator-manager:latest ./artifacts/deployment
+rm -f artifacts/deployment/operator-manager
 
 
