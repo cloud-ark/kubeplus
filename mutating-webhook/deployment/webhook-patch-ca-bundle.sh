39d03d7931ae1e4229318e6af1308e293eed2b9c
Devdatta Kulkarni <devdattakulkarni@gmail.com>
2019-08-13 16:31:27 -0500

    Updated jq parsing for certificate-auth-data and permissions for default serviceaccount

--- mutating-webhook/deployment/webhook-patch-ca-bundle.sh
+++ mutating-webhook/deployment/webhook-patch-ca-bundle.sh
@@ -8,8 +8,9 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+#kubectl config view --raw -o json | sed 's/certificate-authority-data/certificateauthdata/'g | jq -r '.clusters[0].cluster.certificateauthdata'
 
-export CA_BUNDLE=$(kubectl config view --raw --flatten -o json | jq -r '.clusters[] | select(.name == "'$(kubectl config current-context)'") | .cluster."certificate-authority-data"')
+export CA_BUNDLE=$(kubectl config view --raw --flatten -o json |  sed 's/certificate-authority-data/certificateauthdata/'g | jq -r '.clusters[] | select(.name == "'$(kubectl config current-context)'") | .cluster.certificateauthdata')
 
 if command -v envsubst >/dev/null 2>&1; then
     envsubst
