383e74a151d392e88775f360125c756df211f8a1
Devdatta Kulkarni <devdattakulkarni@gmail.com>
2021-08-30 11:54:59 -0500

    Requiring consumer kubeconfig to be provided for opening consumer ui
    - Updated consumer kubeconfig RBAC to include Pod/portforward permission

--- deploy/open-consumer-ui.sh
+++ deploy/open-consumer-ui.sh
@@ -1,12 +1,24 @@
 #!/bin/bash 
 
-KUBEPLUS_POD=`kubectl get pods -A | grep kubeplus | awk '{print $2}'`
-KUBEPLUS_NS=`kubectl get pods -A | grep kubeplus | awk '{print $1}'`
+if (( $# < 1 )); then
+    echo "./open-consumerui.sh <consumer kubeconfig>"
+    exit 0
+fi
 
-kubectl port-forward --address 0.0.0.0 $KUBEPLUS_POD -n $KUBEPLUS_NS 5000:5000 &
+consumerkubecfg=$1
+
+KUBEPLUS_POD=`kubectl get pods -A --kubeconfig=$consumerkubecfg | grep kubeplus | awk '{print $2}'`
+KUBEPLUS_NS=`kubectl get pods -A --kubeconfig=$consumerkubecfg | grep kubeplus | awk '{print $1}'`
+
+kubectl port-forward --address 0.0.0.0 $KUBEPLUS_POD -n $KUBEPLUS_NS 5000:5000 --kubeconfig=$consumerkubecfg &
 
 echo "Consumer UI available at:"
 echo "http://localhost:5000/"
+echo "-----------------------"
+echo "Create service instances by using ffollowing URL:"
+echo "http://localhost:5000/service/<service name>"
+echo "Example:"
+echo "http://localhost:5000/service/HelloWorldService"
 
 
 
