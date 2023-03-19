702b9da795dca10293924c6758c2336f6b150c8e
Emmanuel Nyachoke <achachiez@gmail.com>
2023-03-19 11:46:41 +0300

    Add github actions to build multi arch images

--- deploy/build-artifact-clean.sh
+++ deploy/build-artifact-clean.sh
@@ -7,13 +7,13 @@ fi
 artifacttype=$1
 
 if [ "$artifacttype" = "latest" ]; then
-    docker build -t gcr.io/cloudark-kubeplus/delete-kubeplus-resources:latest -f Dockerfile.cleanup .
+    docker build -t gcr.io/cloudark-kubeplus/delete-kubeplus-resources:latest -f ./Dockerfile.cleanup .
 fi
 
 if [ "$artifacttype" = "versioned" ]; then
     version=`tail -1 versions.txt`
     echo "Building version $version"
-    docker build -t gcr.io/cloudark-kubeplus/delete-kubeplus-resources:$version -f Dockerfile.cleanup .
+    docker build -t gcr.io/cloudark-kubeplus/delete-kubeplus-resources:$version -f ./Dockerfile.cleanup .
     docker push gcr.io/cloudark-kubeplus/delete-kubeplus-resources:$version
 fi
 
--- mutating-webhook/build-artifact.sh
+++ mutating-webhook/build-artifact.sh
@@ -3,21 +3,21 @@
 if (( $# < 1 )); then
     echo "./build-artifact.sh <latest | versioned>"
 fi
-
+cd ../
 artifacttype=$1
 
 if [ "$artifacttype" = "latest" ]; then
-    export GO111MODULE=on; CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o crd-hook
-    docker build --no-cache -t gcr.io/cloudark-kubeplus/pac-mutating-admission-webhook:latest .
+    # export GO111MODULE=on; CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o crd-hook
+    docker build --no-cache -t gcr.io/cloudark-kubeplus/pac-mutating-admission-webhook:latest -f ./mutating-webhook/Dockerfile .
 fi
 
 if [ "$artifacttype" = "versioned" ]; then
     version=`tail -1 versions.txt`
     echo "Building version $version"
-    export GO111MODULE=on; CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o crd-hook
+    # export GO111MODULE=on; CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o crd-hook
     #docker build --no-cache -t lmecld/pac-mutating-admission-webhook:$version .
     #docker push lmecld/pac-mutating-admission-webhook:$version
-    docker build -t gcr.io/cloudark-kubeplus/pac-mutating-admission-webhook:$version .
+    docker build -t gcr.io/cloudark-kubeplus/pac-mutating-admission-webhook:$version  -f ./mutating-webhook/Dockerfile .
     docker push gcr.io/cloudark-kubeplus/pac-mutating-admission-webhook:$version
 fi
 
