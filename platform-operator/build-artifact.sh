590ee4567e0b78372c397200c6b8fd206fc2856c
Emmanuel Nyachoke <achachiez@gmail.com>
2023-03-19 11:46:41 +0300

    Add github actions to build multi arch images

--- platform-operator/build-artifact.sh
+++ platform-operator/build-artifact.sh
@@ -6,21 +6,23 @@ fi
 
 artifacttype=$1
 
+# cd ../
+
 if [ "$artifacttype" = "latest" ]; then
-    CGO_ENABLED=0 export GOOS=linux; go build .
-    #export GOOS=linux; go build .
-    cp platform-operator ./artifacts/deployment/platform-operator
-    docker build -t gcr.io/cloudark-kubeplus/platform-operator:latest ./artifacts/deployment
+    #export GO111MODULE=off; export GOOS=linux; go build .
+    # export GOOS=linux; go build .
+    #cp platform-operator ./artifacts/deployment/platform-operator
+    docker build -t gcr.io/cloudark-kubeplus/platform-operator:latest -f ./artifacts/deployment/Dockerfile .
 fi
 
 if [ "$artifacttype" = "versioned" ]; then
     version=`tail -1 versions.txt`
     echo "Building version $version"
-    CGO_ENABLED=0 export GOOS=linux; go build .
-    #export GOOS=linux; go build .
-    cp platform-operator ./artifacts/deployment/platform-operator
+    #export GO111MODULE=off; export GOOS=linux; go build .
+    # export GOOS=linux; go build .
+    #cp platform-operator ./artifacts/deployment/platform-operator
     #echo "PROJECT_ID $PROJECT_ID"
-    docker build -t gcr.io/cloudark-kubeplus/platform-operator:$version ./artifacts/deployment
+    docker build -t gcr.io/cloudark-kubeplus/platform-operator:$version  -f ./artifacts/deployment/Dockerfile .
     #docker build -t lmecld/platform-operator:$version ./artifacts/deployment
     #docker push lmecld/platform-operator:$version
     docker push gcr.io/cloudark-kubeplus/platform-operator:$version
--- platform-operator/helm-pod/build-artifact.sh
+++ platform-operator/helm-pod/build-artifact.sh
@@ -5,17 +5,18 @@ if (( $# < 1 )); then
 fi
 
 artifacttype=$1
+cd ../../
 
 if [ "$artifacttype" = "latest" ]; then
-    export GO111MODULE=on; CGO_ENABLED=0 export GOOS=linux; go build .
-    docker build --no-cache -t gcr.io/cloudark-kubeplus/helm-pod:latest .
+    export GO111MODULE=on; export GOOS=linux; go build .
+    docker build --no-cache -t gcr.io/cloudark-kubeplus/helm-pod:latest  -f ./platform-operator/helm-pod/Dockerfile .
 fi
 
 if [ "$artifacttype" = "versioned" ]; then
     version=`tail -1 versions.txt`
     echo "Building version $version"
-    export GO111MODULE=on; CGO_ENABLED=0 export GOOS=linux; go build .
-    docker build -t gcr.io/cloudark-kubeplus/helm-pod:$version .
+    export GO111MODULE=on; export GOOS=linux; go build .
+    docker build -t gcr.io/cloudark-kubeplus/helm-pod:$version ./platform-operator/helm-pod/Dockerfile .
     docker push gcr.io/cloudark-kubeplus/helm-pod:$version
 fi
 
