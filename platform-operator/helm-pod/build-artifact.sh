09ad3e3decb28ca2c55cebaf7d98533bb25a6e09
Emmanuel Nyachoke <achachiez@gmail.com>
2023-03-22 15:27:01 +0300

    Comment  export GO111MODULE in helm pod

--- platform-operator/helm-pod/build-artifact.sh
+++ platform-operator/helm-pod/build-artifact.sh
@@ -8,14 +8,14 @@ artifacttype=$1
 cd ../../
 
 if [ "$artifacttype" = "latest" ]; then
-    export GO111MODULE=on; export GOOS=linux; go build .
+    # export GO111MODULE=on; export GOOS=linux; go build .
     docker build --no-cache -t gcr.io/cloudark-kubeplus/helm-pod:latest  -f ./platform-operator/helm-pod/Dockerfile .
 fi
 
 if [ "$artifacttype" = "versioned" ]; then
     version=`tail -1 versions.txt`
     echo "Building version $version"
-    export GO111MODULE=on; export GOOS=linux; go build .
+    # export GO111MODULE=on; export GOOS=linux; go build .
     docker build -t gcr.io/cloudark-kubeplus/helm-pod:$version ./platform-operator/helm-pod/Dockerfile .
     docker push gcr.io/cloudark-kubeplus/helm-pod:$version
 fi
