677b1346d9e2254d67879ae23a28e1f1632011dc
Dev <732525+devdattakulkarni@users.noreply.github.com>
2024-05-10 16:12:19 -0500

    More cleanup (#1270)

--- scripts/setpaths.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-export GOROOT=/usr/local/go
-export PATH=/usr/local/go/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin
-export GOPATH=$HOME/go
-export PATH=$PATH:$HOME/go/src/k8s.io/kubernetes/cluster
