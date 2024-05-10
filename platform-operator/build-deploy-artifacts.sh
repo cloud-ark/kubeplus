9243351ee0c4aceccc231d0828a7e54173fca798
Devdatta Kulkarni <devdattakulkarni@gmail.com>
2024-05-10 20:07:46 +0000

    platform-operator cleanup
    removed unused files/folders

--- platform-operator/build-deploy-artifacts.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-export GOOS=linux; go build .
-cp platform-operator ./artifacts/deployment/platform-operator
-docker build -t lmecld/platform-operator:0.0.3 ./artifacts/deployment
-
-
