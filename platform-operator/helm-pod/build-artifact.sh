7bb627254f45f7e2bbd391cc5ea8cf118b37586c
Dev <732525+devdattakulkarni@users.noreply.github.com>
2024-06-12 05:59:06 -0500

    GitHub Action update (#1307)

--- platform-operator/helm-pod/build-artifact.sh
+++ platform-operator/helm-pod/build-artifact.sh
@@ -7,14 +7,14 @@ fi
 artifacttype=$1
 
 if [ "$artifacttype" = "latest" ]; then
-    export GO111MODULE=on; export GOOS=linux; go build .
+    export GO111MODULE=on; CGO_ENABLED=0 export GOOS=linux; go build .
     docker build --no-cache -t gcr.io/cloudark-kubeplus/helm-pod:latest .
 fi
 
 if [ "$artifacttype" = "versioned" ]; then
     version=`tail -1 versions.txt`
     echo "Building version $version"
-    export GO111MODULE=on; export GOOS=linux; go build .
+    export GO111MODULE=on; CGO_ENABLED=0 export GOOS=linux; go build .
     docker build -t gcr.io/cloudark-kubeplus/helm-pod:$version .
     docker push gcr.io/cloudark-kubeplus/helm-pod:$version
 fi
